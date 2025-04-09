1687bbc6301cba22b3216af1f9f32ec02b48939b
vaibhavibelamkar <vaibhavi.belamkar22@gmail.com>
2025-04-09 11:25:40 +0530

    navbar fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           {/* Logo and Website Name */}
-          <div className="flex items-center space-x-3 ml-[-120px]">
+          <div className="flex items-center space-x-3">
             <GraduationCap className="h-8 w-8 text-white" />
             <span className="text-white font-bold text-xl">
               FindMyScholarship
